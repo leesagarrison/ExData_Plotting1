61e1d633850cfae72109f0cd34908b8b65eff9b7
Leesa Garrison <leesa.langvin@gmail.com>
2014-05-10 23:38:16 -0700

    Plot 4

--- plot4.R
+++ plot4.R
@@ -1,3 +1,26 @@
+##load consumption data
+filename<-"household_power_consumption.txt"
+
+##Fastread the first column only in order to subset by date 
+getRows <- fread(filename, header=TRUE, select=1)
+
+##All = those rows where the dates are Feb1 or Feb2, 2007
+all<-(which(getRows$Date %in% c("1/2/2007", "2/2/2007")))
+
+##get the index for the first record, and the number of records to keep
+skipLines<-min(all)
+rowsToKeep<-length(all)
+
+##Grab the column headers by doing a fast read of first line
+consumptionHeader<-names(fread(filename,nrows=1, header=TRUE))
+
+##read consumption data for only those records in the right daterange
+consumptionData<-read.table(filename,skip=(skipLines-1), nrows=rowsToKeep, na.strings="?", sep=";", col.names=consumptionHeader)
+
+##clear getRows from memory
+rm(getRows) 
+
+
 par(mfrow = c(2,2))                        #set canvas for 4 plots
 
 ##plot Global active powerin upper left corner of canvas
